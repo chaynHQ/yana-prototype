23d4ff88b65fdf109d231463652aa2b0107295a5
Jits <dev@jits.co.uk>
2020-01-14 14:56:03 +0000

    Latest

--- components/ProcessSectionContent.vue
+++ components/ProcessSectionContent.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-if="section.processMap && section.processMap.length">
-      <h3 class="subtitle-2 font-weight-bold mb-2 text-center">
+      <h3 class="subtitle-2 font-weight-bold mb-1 text-center">
         <v-chip label color="pink lighten-5">
           Process
         </v-chip>
@@ -13,12 +13,12 @@
           :key="index"
           class="process-section-step"
         >
-          <v-expansion-panel-header class="subtitle-2" color="pink lighten-5">
+          <v-expansion-panel-header class="subtitle-2" color="transparent">
             <div class="process-section-step-header">
               {{ step.name }}
             </div>
           </v-expansion-panel-header>
-          <v-expansion-panel-content color="pink lighten-5">
+          <v-expansion-panel-content color="transparent">
             <div class="process-section-step-content">
               <vue-simple-markdown
                 :source="step.text"
@@ -30,57 +30,37 @@
       </v-expansion-panels>
     </div>
 
-    <div v-if="section.resources && section.resources.length" class="mt-5">
-      <h3 class="subtitle-2 font-weight-bold mb-2 text-center">
-        <v-chip label color="indigo lighten-5">
+    <div v-if="section.resources && section.resources.length" class="mt-4">
+      <h3 class="subtitle-2 font-weight-bold mb-1 text-center">
+        <v-chip label color="pink lighten-5">
           Resources
         </v-chip>
       </h3>
 
-      <v-list dense color="indigo lighten-5">
-        <template v-for="(resource, index) in section.resources">
-          <v-list-item :key="resource.name">
-            <v-list-item-content>
-              <v-list-item-title>
-                <a :href="resource.url">{{ resource.name }}</a>
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-
-          <v-divider
-            v-if="index + 1 < section.resources.length"
-            :key="index"
-          ></v-divider>
-        </template>
-      </v-list>
+      <ul class="simple-list body-2">
+        <li v-for="(resource, index) in section.resources" :key="index">
+          <a :href="resource.url">{{ resource.name }}</a>
+        </li>
+      </ul>
     </div>
 
-    <div v-if="section.checklist && section.checklist.length" class="mt-5">
-      <h3 class="subtitle-2 font-weight-bold mb-2 text-center">
-        <v-chip label color="green lighten-5">
+    <div v-if="section.checklist && section.checklist.length" class="mt-4">
+      <h3 class="subtitle-2 font-weight-bold mb-1 text-center">
+        <v-chip label color="pink lighten-5">
           Checklist
         </v-chip>
       </h3>
 
-      <v-list dense color="green lighten-5">
-        <template v-for="(item, index) in section.checklist">
-          <v-list-item :key="item.text">
-            <v-list-item-action>
-              <v-checkbox v-model="item.done" color="primary"></v-checkbox>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>
-                {{ item.text }}
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-
-          <v-divider
-            v-if="index + 1 < section.checklist.length"
-            :key="index"
-          ></v-divider>
-        </template>
-      </v-list>
+      <ul class="simple-list checklist body-2">
+        <li v-for="(item, index) in section.checklist" :key="index">
+          <v-checkbox
+            v-model="item.done"
+            :label="item.text"
+            color="primary"
+            hide-details
+          ></v-checkbox>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
