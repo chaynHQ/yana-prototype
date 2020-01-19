822805a8cb2604ffdc8961e2a396cd89c9039854
Jits <dev@jits.co.uk>
2020-01-19 16:33:17 +0000

    Latest

--- components/Incident.vue
+++ components/Incident.vue
@@ -311,109 +311,109 @@ export default {
   },
   mounted() {
     // ONLY FOR TESTING:
-    this.events = [
-      {
-        id: nanoid(8),
-        what: {},
-        when: {
-          dontKnow: true
-        },
-        where: {},
-        people: {},
-        evidence: {}
-      },
-      {
-        id: nanoid(8),
-        what: {},
-        when: {},
-        where: {},
-        people: {},
-        evidence: {}
-      },
-      {
-        id: nanoid(8),
-        what: {},
-        when: {
-          date: '2019-01-03',
-          approximate: true
-        },
-        where: {},
-        people: {},
-        evidence: {}
-      },
-      {
-        id: nanoid(8),
-        what: {},
-        when: {
-          date: '2019-01-01',
-          time: '13:00'
-        },
-        where: {},
-        people: {},
-        evidence: {}
-      },
-      {
-        id: nanoid(8),
-        what: {},
-        when: {
-          date: '2019-01-01'
-        },
-        where: {},
-        people: {},
-        evidence: {}
-      },
-      {
-        id: nanoid(8),
-        what: {},
-        when: {
-          date: '2019-01-01',
-          time: '11:00'
-        },
-        where: {},
-        people: {},
-        evidence: {}
-      },
-      {
-        id: nanoid(8),
-        what: {},
-        when: {
-          date: '2019-01-05',
-          time: '19:00'
-        },
-        where: {},
-        people: {},
-        evidence: {}
-      },
-      {
-        id: nanoid(8),
-        what: {},
-        when: {},
-        where: {},
-        people: {},
-        evidence: {}
-      },
-      {
-        id: nanoid(8),
-        what: {},
-        when: {
-          dontKnow: true
-        },
-        where: {},
-        people: {},
-        evidence: {}
-      },
-      {
-        id: nanoid(8),
-        what: {},
-        when: {
-          date: '2019-01-01',
-          time: '12:00'
-        },
-        where: {},
-        people: {},
-        evidence: {}
-      }
-    ]
+    // this.events = [
+    //   {
+    //     id: nanoid(8),
+    //     what: {},
+    //     when: {
+    //       dontKnow: true
+    //     },
+    //     where: {},
+    //     people: {},
+    //     evidence: {}
+    //   },
+    //   {
+    //     id: nanoid(8),
+    //     what: {},
+    //     when: {},
+    //     where: {},
+    //     people: {},
+    //     evidence: {}
+    //   },
+    //   {
+    //     id: nanoid(8),
+    //     what: {},
+    //     when: {
+    //       date: '2019-01-03',
+    //       approximate: true
+    //     },
+    //     where: {},
+    //     people: {},
+    //     evidence: {}
+    //   },
+    //   {
+    //     id: nanoid(8),
+    //     what: {},
+    //     when: {
+    //       date: '2019-01-01',
+    //       time: '13:00'
+    //     },
+    //     where: {},
+    //     people: {},
+    //     evidence: {}
+    //   },
+    //   {
+    //     id: nanoid(8),
+    //     what: {},
+    //     when: {
+    //       date: '2019-01-01'
+    //     },
+    //     where: {},
+    //     people: {},
+    //     evidence: {}
+    //   },
+    //   {
+    //     id: nanoid(8),
+    //     what: {},
+    //     when: {
+    //       date: '2019-01-01',
+    //       time: '11:00'
+    //     },
+    //     where: {},
+    //     people: {},
+    //     evidence: {}
+    //   },
+    //   {
+    //     id: nanoid(8),
+    //     what: {},
+    //     when: {
+    //       date: '2019-01-05',
+    //       time: '19:00'
+    //     },
+    //     where: {},
+    //     people: {},
+    //     evidence: {}
+    //   },
+    //   {
+    //     id: nanoid(8),
+    //     what: {},
+    //     when: {},
+    //     where: {},
+    //     people: {},
+    //     evidence: {}
+    //   },
+    //   {
+    //     id: nanoid(8),
+    //     what: {},
+    //     when: {
+    //       dontKnow: true
+    //     },
+    //     where: {},
+    //     people: {},
+    //     evidence: {}
+    //   },
+    //   {
+    //     id: nanoid(8),
+    //     what: {},
+    //     when: {
+    //       date: '2019-01-01',
+    //       time: '12:00'
+    //     },
+    //     where: {},
+    //     people: {},
+    //     evidence: {}
+    //   }
+    // ]
   },
   methods: {
     formatDate(value) {
