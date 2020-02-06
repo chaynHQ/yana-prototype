29273788740578580f4d13f5871c9655930fb744
Jits <dev@jits.co.uk>
2020-02-06 14:52:15 +0000

    Latest

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,56 @@
 <template>
   <section id="about">
     <div class="px-3">
-      <h1 class="headline">About Us</h1>
-      <h2>Coming soon!</h2>
+      <h1 class="headline">About us</h1>
+
+      <p class="body-1 mt-3">
+        yana provides survivors of sexual assault with information and
+        connections to support that they need to understand their experience and
+        make informed decisions.
+      </p>
+
+      <p class="body-1">
+        Though information exists on what to do and where to go, it’s often
+        fragmented and hard to navigate. Stigma, fear of being judged, and a
+        lack of trust in institutions, especially among vulnerable groups, also
+        contribute to survivors not coming forward.
+      </p>
+
+      <p class="body-1">
+        Co-created with survivors, yana brings together timely and accurate
+        information in one safe and accessible place.
+      </p>
+
+      <p class="body-1">
+        yana’s recording tool provides a structured approach to logging details
+        of the assault. This can significantly help a later investigation and
+        may remove the need for survivors to repeat the same details to multiple
+        authorities – something that we know can be traumatic. yana walks hand
+        in hand with survivors through the reporting and justice process, and
+        makes connections to support services.
+      </p>
+
+      <p class="body-1">
+        In developing yana, survivors have been able to contribute to supporting
+        others; something they have told us is an essential part of their
+        recovery.
+      </p>
+
+      <p class="body-1">
+        yana is part of
+        <a href="https://chayn.co/" target="_blank" rel="noopener">Chayn</a> - a
+        global volunteer network that provides online resources to help
+        survivors tackle gender-based violence.
+      </p>
+
+      <h2 class="title mt-3">
+        Get in touch
+      </h2>
+
+      <p class="body-1 mt-3">
+        We’re always looking to improve and would love to know what you think of
+        yana. Please get in touch with us at team@chayn.co
+      </p>
     </div>
   </section>
 </template>
