a4874ab85ef2c7d84ad19d11aa4006424077ee54
Jits <dev@jits.co.uk>
2020-02-06 15:04:23 +0000

    Latest

--- pages/partners.vue
+++ pages/partners.vue
@@ -2,7 +2,64 @@
   <section id="partners">
     <div class="px-3">
       <h1 class="headline">Partners</h1>
-      <h2>Coming soon!</h2>
+
+      <p class="body-1 mt-3">
+        yana is a groundbreaking digital platform that will empower survivors of
+        sexual assault with accurate and timely support information, a space to
+        record what happened and critical checklists of reporting and justice
+        procedures.
+      </p>
+
+      <p class="body-1">
+        We’re keen to talk to sexual assault charities and support organisations
+        about how we can work together – particularly on yana’s support
+        information. This is a priority for us – we want to make sure only
+        quality, accurate information makes it onto the platform. Please get in
+        touch with us so we can discuss how we can collaborate.
+      </p>
+
+      <p class="body-1">
+        Giving clarity and control when survivors need it the most, yana will be
+        the world’s largest collaborative resource for survivors of sexual
+        assault, launching initially in the UK and India.
+      </p>
+
+      <p class="body-1">
+        yana will offer signposting to local shelters – including those
+        supporting marginalised communities – sexual violence units, lawyers,
+        therapists, digital services and more. There will be information on
+        procedures for reporting so that survivors know what to expect but also
+        alerts them if proper procedure is not followed.
+      </p>
+
+      <p class="body-1">
+        Importantly, yana’s recording tool will provide a space for survivors to
+        enter and save details of the incident. This can help a later
+        investigation and save survivors the trouble of repeating details to
+        multiple authorities.
+      </p>
+
+      <h2 class="title mt-3">
+        Donate
+      </h2>
+
+      <p class="body-1 mt-3">
+        If you’d like to get involved in supporting yana, we’d love to hear from
+        you. Please get in touch with us at team@chayn.co
+      </p>
+
+      <h2 class="title mt-3">
+        Newsletter
+      </h2>
+
+      <p class="body-1 mt-3">
+        For staying in the loop for when the project launches, contributing
+        information, sharing your expertise as a survivor and taking part in the
+        crowdfunding — sign up to
+        <a href="https://chayn.co/newsletter/" target="_blank" rel="noopener"
+          >our newsletter</a
+        >.
+      </p>
     </div>
   </section>
 </template>
